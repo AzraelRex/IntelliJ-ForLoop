2bc060e48b75119788a03109bd1d1b652a3bb028
Azrael Rex Anacleto <anacletoazraelrex@gmail.com>
2024-06-20 22:47:54 +0800

    Changed the integer from 9 to 11.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 class Main {
     public static void main(String[] args) {
 
-        int n = 11;
+        int n = 10;
         // for loop test
         for (int i = 0; i <= n; ++i) {
             System.out.println(i);
